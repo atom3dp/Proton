307ae9557ee2dd68dae05063741016ea279d5cd6
Phil Xing <philxing1967@gmail.com>
2018-07-29 03:57:36 +0800

    Change the motherboard type
    1. Replace heater bed with cooling fan for D8 MOSFET.
    2. Assign UUID to this machine.
    3. Simplify the machine name.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -121,16 +121,16 @@
 // The following define selects which electronics board you have.
 // Please choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_RAMPS_14_EFB
+  #define MOTHERBOARD BOARD_RAMPS_14_EFF
 #endif
 
 // Optional custom name for your RepStrap or other custom machine
 // Displayed in the LCD "Ready" message
-#define CUSTOM_MACHINE_NAME "ATOM_Proton.01"
+#define CUSTOM_MACHINE_NAME "ATOM Proton"
 
 // Define this to set a unique identifier for this printer, (Used by some programs to differentiate between machines)
 // You can use an online service to generate a random UUID. (eg http://www.uuidgenerator.net/version4)
-//#define MACHINE_UUID "00000000-0000-0000-0000-000000000000"
+#define MACHINE_UUID "7e76421b-df59-4e68-8c77-13eb567fae88"
 
 // This defines the number of extruders
 // :[1,2,3,4]
