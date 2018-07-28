a2c762fd18b300f6858ab323258b0a84b42b8fce
Phil Xing <philxing1967@gmail.com>
2018-07-29 03:53:49 +0800

    Correct the Probe offset
    X=-32, Y=-21

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -476,8 +476,8 @@
 //    |           |
 //    O-- FRONT --+
 //  (0,0)
-#define X_PROBE_OFFSET_FROM_EXTRUDER  0  // ??  X offset: -left  +right  [of the nozzle]
-#define Y_PROBE_OFFSET_FROM_EXTRUDER  0  // ??  Y offset: -front +behind [the nozzle]
+#define X_PROBE_OFFSET_FROM_EXTRUDER  -32  // ??  X offset: -left  +right  [of the nozzle]
+#define Y_PROBE_OFFSET_FROM_EXTRUDER  -21  // ??  Y offset: -front +behind [the nozzle]
 #define Z_PROBE_OFFSET_FROM_EXTRUDER  0 //??test   // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
